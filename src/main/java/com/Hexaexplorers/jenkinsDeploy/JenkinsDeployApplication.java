2b4cc6f2f90a06bfb126bae776e220f3d1833055
VIJAY KUMAR <vijaykumar@VIJAYs-MacBook-Pro.local>
2024-07-26 19:34:34 +0530

    first commit

---
+++ src/main/java/com/Hexaexplorers/jenkinsDeploy/JenkinsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.Hexaexplorers.jenkinsDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hexaexplorers/jenkinsDeploy/JenkinsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hexaexplorers.jenkinsDeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
