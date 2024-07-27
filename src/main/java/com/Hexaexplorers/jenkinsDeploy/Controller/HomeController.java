a065bf0e15176899dea9228e7064172b9d8e56b5
vijaykumar <hexaexplorers@gmail.com>
2024-07-27 18:38:05 +0530

    try catch

--- src/main/java/com/Hexaexplorers/jenkinsDeploy/Controller/HomeController.java
+++ src/main/java/com/Hexaexplorers/jenkinsDeploy/Controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.Hexaexplorers.jenkinsDeploy.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api")
 public class HomeController {
 
     @GetMapping("/")
